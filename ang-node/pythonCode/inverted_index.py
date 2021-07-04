24b5a2edcc5469b86729d83797c704f5db0c5124
JohanTv <johan.tanta@utec.edu.pe>
2021-07-04 16:56:16 -0500

    advance

--- ang-node/pythonCode/inverted_index.py
+++ ang-node/pythonCode/inverted_index.py
@@ -12,23 +12,30 @@ from decimal import Decimal
 import regex
 import numpy as np
 from collections import Counter
+import pickle
+
+bin_path = './bin/'
+
+term_termid_dict_file = open(bin_path + "term_termid_dict.dat", "rb")
+tweet_termids_dict_file = open(bin_path + "tweet_termids_dict.dat", "rb")
+filenameid_filename_dict_file = open(bin_path + "filenameid_filename_dict.dat", "rb")
+termid_df_dict_file = open(bin_path + "termid_df_dict.dat", "rb")
+N_file = open(bin_path + "N.dat", "rb")
+NTerms_file = open(bin_path + "NTerms.dat", "rb")
 
 #nltk.download('punkt')
 #nltk.download('stopwords')
 
 # define N objets
-N = 100
-tweet1 = dict()
-tweets = dict()
+N = pickle.load(N_file)
+NTerms = pickle.load(NTerms_file)
+termid_df_dict = pickle.load(termid_df_dict_file)
+tweet_termids_dict = pickle.load(tweet_termids_dict_file)
+term_termid_dict = pickle.load(term_termid_dict_file)
+filenameid_filename_dict = pickle.load(filenameid_filename_dict_file)
 
-class tokenObject:
-    def __init__(self):
-        self.tf_per_tweet = {} # Term frecuency on text per tweet
-        self.df = 0 # Total tweet frecuency
-        self.tf = 0 # Total term frecuency
-
-def stopwords_stemmer(filename_tweet_text):
-    tokens = nltk.word_tokenize(filename_tweet_text)
+def stopwords_stemmer(tweet_text):
+    tokens = nltk.word_tokenize(tweet_text)
     stoplist = stopwords.words("spanish")
     stoplist += ['/…','--','RT','`','@','|','¿','?', '¡', '!','aqui', '.', ',', ';', '«', '»', ':', '(', ')', '"','en', 'En', 'la', 'La', 'con', 'Con','sin', 'Sin', 'al', 'Al', 'de', 'De', 'el', 'El', '#', '$', '^', '&', '*', '%']
     tokens_clean = tokens.copy()
@@ -38,113 +45,69 @@ def stopwords_stemmer(filename_tweet_text):
             tokens_clean.remove(token)
             
     stemmer = SnowballStemmer("spanish")
-    for token in tokens_clean:
-        token = stemmer.stem(token)
+    for i in range(len(tokens_clean)):
+        tokens_clean[i] = stemmer.stem(tokens_clean[i])
 
     return tokens_clean
 
-json_path = './data/'
-tweets = os.listdir(json_path)
-inverted_index={}
-with open(json_path + tweets[0], encoding="utf8") as file:
-    json_content = json.load(file)
-    for tweet in json_content:
-        tokens_clean = stopwords_stemmer(tweet["text"])
-        for token in tokens_clean:
-            if token not in inverted_index:
-                inverted_index[token] = tokenObject()
-                inverted_index[token].tf += 1
-                if tweet["id"] not in inverted_index[token].tf_per_tweet:
-                    inverted_index[token].tf_per_tweet[tweet["id"]] = 1
-                    inverted_index[token].df += 1
-                else:
-                    inverted_index[token].tf_per_tweet[tweet["id"]] += 1
 
-
-
-# def get_frecuency_terms(terms):
-#     terms_dict = dict()
-#     for term in terms:
-#         if term not in terms_dict:
-#             terms_dict[term] = 1
-#         else:
-#             terms_dict[term] += 1
-#     return terms_dict
-
-##
-#   {   
-#       keiko : { 12312312: 3, 421412412, 6},
-#       tramposa: {12312312 : 1, 3123124: 5}        
-#        
-#   }
-#  
-##
-
-# def readTweets(tweets):
-#     term_dict = dict()
-#     for tweet in tweets:
-#         token_clean = stopwords_stemmer(tweet["text"])
-#         id_tweet = tweet["id"] # los id son únicos?
-#         terms_with_frequency = Counter(token_clean)
-#         for term, freq in terms_with_frequency:
-#             if(term not in term_dict):
-#                 term_dict[term] = dict()
-#                 term_dict[term][id_tweet] = freq
-#             else:
-#                 term_dict[term][id_tweet] = freq
-#     return term_dict
-
-
-def createVectorTf_idf(tweet):
-    vector_tf_idf = np.zeros(N)
-    i = 0
-    for termId in tweet1.key():
-        vector_tf_idf[termId] = tweet1[termId]
+def createVectorTF_IDF(tweet):
+    vector_tf_idf = np.zeros(NTerms)
+    for term_id in tweet:
+        vector_tf_idf[term_id] = tweet[term_id]
     return vector_tf_idf
 
 def parser(query):
     query_clean = stopwords_stemmer(query)
-    tf_words_query = Counter(query_clean)
-    indice_invertido_dict = open("index_invertido.dat", "r")
-    term_termId_dict = open("terms.data", "r")
-    result = dict()
-    for word, frec in tf_words_query.items():
-        if(word in indice_invertido_dict):
-            tf = frec
-            df = len(indice_invertido_dict[term_termId_dict[word]])
-            tf_idf = math.log(1+tf, 10)* math.log(N/df,10)
-            result[term_termId_dict[word]] = tf_idf
+    each_term_frequency = Counter(query_clean)
+    result = {}
+    for term in each_term_frequency:
+        if(term in term_termid_dict):
+            term_id = term_termid_dict[term]
+            tf = each_term_frequency[term]
+            df = termid_df_dict[term_id]
+            tf_idf = math.log(1 + tf, 10)* math.log(N/df,10)
+            result[term_id] = tf_idf
     return result
     
 def cosine(q, doc):
-    return np.dot(q,doc)/(np.linalg.norm(q)* np.linalg.norm(doc))
-
+    r = np.dot(q,doc)/(np.linalg.norm(q) * np.linalg.norm(doc))
+    #if(r > 0):
+    #    print(r)
+    return r
 # Nos trae los (id, cosine_value) menor a mayor. Menor distancia mayor 
 def searchKNN(query, k):
     pq = PriorityQueue()
-    v_query = createVectorTf_idf(parser(query))
-    for id, value in tweets.items():
-        v = createVectorTf_idf(value)
+    v_query = createVectorTF_IDF(parser(query))
+    for tweet_id in tweet_termids_dict:
+        v = createVectorTF_IDF(tweet_termids_dict[tweet_id][1])
         valor = cosine(v_query, v)
-        pq.put((valor, id))
-    result = []
-    while pq.empty()!= 1:
+        if(pq.qsize() < k):
+            pq.put((valor, tweet_id))
+        else:
+            top = pq.get()
+            if(valor > top[0]):
+                pq.put((valor, tweet_id))
+            else:
+                pq.put(top)
+    result = [0] * k
+    i = k - 1
+    while not pq.empty():
         data = pq.get()
-        result.append(data)
-    return result[:k]
+        result[i] = data
+        i -= 1
+    return result
 
 
 def retrieve_tweet(docId):
-    json_path = './data/'
-    tweets = os.listdir(json_path)
-    with open(json_path + tweets[0], encoding="utf8") as file:
+    json_path = './data2/'
+    filenameid = tweet_termids_dict[docId][0]
+    filename = filenameid_filename_dict[filenameid]
+    with open(json_path + filename, encoding="utf8") as file:
         json_content = json.load(file)
-        for json_str in json_content:
-            id = str(json_str['id'])
-            if id == docId:
-                return json.dumps(json_str) 
-                
-
+        for tweet in json_content:
+            if tweet['id'] == docId:
+                return json.dumps(tweet) 
 
 def retrieve_tweets(query,k):
     res = []
